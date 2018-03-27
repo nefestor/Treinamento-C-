d0f8c4ad8e8dd9743b3ef2101c41fb6746f2117b
Tiago Coelho <tiago.pereira@forlogic.net>
2018-03-27 10:31:19 -0300

    Altera variável Title

--- Projetinho/Base.cs
+++ Projetinho/Base.cs
@@ -8,7 +8,7 @@ namespace Projetinho
 {
     public class Base : iBase
     {
-        public string Title { get; set; }
+        private string Title { get; set; }
         public string Comments { get; set; }
         public int Id { get; set; }
         public bool Removed { get; set; }
